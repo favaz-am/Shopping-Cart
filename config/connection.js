9ea59c1a564f50b4b13c692db49c0b79015b75c6
favaz-am <favazam98@gmail.com>
2026-08-04 00:42:13 +0530

    fix: use MongoDB Atlas connection for Render deployment

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,24 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017'
-const dbname = process.env.DB_NAME || 'shopping'
+const url = process.env.MONGODB_URI;
 
 let state = {
-    db: null
-}
+  db: null
+};
 
-module.exports.connect = function(done) {
-    MongoClient.connect(url)
-        .then((client) => {
-            state.db = client.db(dbname)
-            console.log('DB Connected Successfully')
-            done()
-        })
-        .catch((err) => {
-            console.error('DB Connection Failed', err)
-            done(err)
-        })
-}
+module.exports.connect = function (done) {
+  MongoClient.connect(url)
+    .then((client) => {
+      state.db = client.db(); // uses the database name from MONGODB_URI
+      console.log('DB Connected Successfully');
+      done();
+    })
+    .catch((err) => {
+      console.error('DB Connection Failed:', err);
+      done(err);
+    });
+};
 
-module.exports.get = function() {
-    return state.db
-}
+module.exports.get = function () {
+  return state.db;
+};
